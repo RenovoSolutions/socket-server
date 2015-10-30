082d269bc1d40a23c98cbe0b1975dda78ecb1684
Josh Graber <jgraber@renovo1.com>
2015-10-30 16:44:26 -0400

    Add a simple module for connecting to a socket and emitting messages to it.

---
+++ socket.js
@@ -0,0 +1,24 @@
+var PORT = 9002;
+var Server = require('socket.io');
+
+exports.connect = function(port) {
+	if (!port) {
+		port = PORT;
+	}
+
+	var io = new Server(port);
+	io.on('connection', function(socket) {
+		console.log('- somebody connected -');
+
+		socket.on('disconnect', function() {
+			console.log('- somebody disconnected -');
+		});
+	});
+
+	return {
+		send: function(channel, message) {
+			console.log('- sending "' + message + '" to "' + channel + '" -');
+			io.emit(channel, message);
+		},
+	};
+}
