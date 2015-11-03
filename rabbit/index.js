e251d6124c279746f508ebd42a794b2fc146a08c
Josh Graber <jgraber@renovo1.com>
2015-11-03 10:41:17 -0500

    Move rabbit files into a separate folder. The rabbit index file now contains the queue file and returns a stream of data loaded from the configured queue.

---
+++ rabbit/index.js
@@ -0,0 +1,29 @@
+var rabbit = require('amqp');
+var Rx = require('rx');
+
+var config = require('./rabbit.json');
+
+var getQueue = require('./queue');
+
+exports.connect = function() {
+	var queueStream = new Rx.Subject();
+
+	var connection = rabbit.createConnection({ host: config.host, login: config.login, password: config.password });
+	Rx.Observable.fromEvent(connection, 'ready').subscribe(function() {
+		console.log('- connected to rabbit -');
+
+		getQueue(connection).subscribe(function(queue) {
+			console.log('- queue is listening for messages -');
+			queue.subscribe(function(message, headers) {
+				queueStream.onNext({
+					channel: headers.channel,
+					body: message.data.toString()
+				});
+			});
+		}, function(error) {
+			queueStream.onError(error);
+		});
+	});
+
+	return queueStream;
+};
--- rabbit/queue.js
+++ rabbit/queue.js
@@ -1,19 +1,19 @@
 var guid = require('uuid');
-var q = require('q');
+var Rx = require('rx');
 
 var config = require('./rabbit.json');
 
 module.exports = function(connection) {
-	var deferred = q.defer();
+	var queueSubject = new Rx.Subject();
 	var name = guid.v1();
 
 	connection.queue(name, { exclusive: true }, function(queue) {
 		console.log('- queue created -');
 		queue.bind(config.exchange, '', function() {
 			console.log('- binding queue -');
-			deferred.resolve(queue);
+			queueSubject.onNext(queue);
 		});
 	});
 
-	return deferred.promise;
+	return queueSubject;
 };
