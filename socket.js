eb10f10c2f7ebc9145d61948b1d926de69253229
Josh Graber <jgraber@renovo1.com>
2015-11-03 11:55:48 -0500

    Add the queue logger and log information about who connected and how many connections there are.

--- socket.js
+++ socket.js
@@ -1,6 +1,18 @@
 var PORT = 80;
 var Server = require('socket.io');
 
+var logger = require('node-queue-logger');
+var rabbitConfig = require('./rabbit').config;
+
+logger.configure({
+	host: rabbitConfig.host,
+	login: rabbitConfig.login,
+	password: rabbitConfig.password,
+	exchange: 'log',
+});
+
+var totalConnections = 0;
+
 exports.connect = function(port) {
 	if (!port) {
 		port = PORT;
@@ -8,10 +20,28 @@ exports.connect = function(port) {
 
 	var io = new Server(port);
 	io.on('connection', function(socket) {
-		console.log('- somebody connected -');
+		totalConnections++;
+		logger.info({
+			area: 'sockets',
+			topic: 'connections',
+			type: 'connect',
+			address: socket.conn.remoteAddress,
+			connections: totalConnections,
+		});
+		console.log('- somebody connected from ' + socket.conn.remoteAddress + ' -');
+		console.log('- ' + totalConnections + ' total connections -');
 
 		socket.on('disconnect', function() {
-			console.log('- somebody disconnected -');
+			totalConnections--;
+			logger.info({
+				area: 'sockets',
+				topic: 'connections',
+				type: 'disconnect',
+				address: socket.conn.remoteAddress,
+				connections: totalConnections,
+			});
+			console.log('- somebody disconnected from ' + socket.conn.remoteAddress + ' -');
+			console.log('- ' + totalConnections + ' total connections -');
 		});
 	});
 
