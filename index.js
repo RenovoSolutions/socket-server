a78aa48a7f663f0f17836a2950d529f14aeaeea4
Josh Graber <jgraber@renovo1.com>
2015-11-03 10:43:04 -0500

    Update rabbit consumption code to use the new simplified stream of messages from the queue.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var rabbit = require('./rabbit');
-var config = require('./rabbit.json');
 var commandLineArgs = require('command-line-args');
 
 var args = commandLineArgs([{ name: 'port', alias: 'p' }]).parse();
@@ -7,21 +6,12 @@ var args = commandLineArgs([{ name: 'port', alias: 'p' }]).parse();
 var socketConfig = require('./socket');
 var socketInst = null;
 
-var queue = require('./queue');
-
-rabbit.connect().then(function(connection) {
-	queue(connection).then(function(queue) {
-		console.log('- queue is listening for messages -');
-		queue.subscribe(function(message, headers) {
-			push(headers.channel, message.data.toString());
-		});
-	});
-});
-
 socketInst = socketConfig.connect(args.port);
 
-function push(channel, message) {
+rabbit.connect().subscribe(function(message) {
 	if (socketInst) {
-		socketInst.send(channel, message);
+		socketInst.send(message.channel, message.body);
 	}
-}
+}, function() {
+	console.error('- failed to connect to the queue -');
+});
